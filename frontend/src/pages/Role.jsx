abaa7b0f10fca0de0b5485f8a99e65da1d379d9e
ajinkya <ajaj51572@gmail.com>
2024-07-20 10:22:43 +0530

    update

--- frontend/src/pages/Role.jsx
+++ frontend/src/pages/Role.jsx
@@ -1,69 +1,66 @@
-import React , {useState} from "react";
+import React, { useState } from "react";
 import { IoMdClose } from "react-icons/io";
 import axios from "axios";
 import { Api } from "../common";
-import {toast} from "react-toastify";
+import { toast } from "react-toastify";
 
-const Role = ({users,onClose,refresh}) => {
-   
-    const {_id,name,email,role} = users;
-    console.log("users",users);
+const Role = ({ users, onClose, refresh }) => {
+  const { _id, name, email, role } = users;
+  console.log("users", users);
 
-    const [userRole,setUserRole]=useState(role);
-      console.log("userRole",userRole);
-
+  const [userRole, setUserRole] = useState(role);
+  console.log("userRole", userRole);
+  // hgjhfdgjdfjg
   const roleCheck = {
-     admin:"admin",
-    user:"user",
+    admin: "admin",
+    user: "user",
   };
 
-  const updateUser = async()=>{
+  const updateUser = async () => {
     try {
-        const payload ={
-            id:_id,
-            name,
-            email,
-            role:userRole 
-        };
+      const payload = {
+        id: _id,
+        name,
+        email,
+        role: userRole,
+      };
 
-        const {data} = await  axios.post(Api.updateAdmin.url,payload,{
-            withCredentials:true
+      const { data } = await axios.post(Api.updateAdmin.url, payload, {
+        withCredentials: true,
+      });
+
+      if (data?.status) {
+        toast.success(data?.message, {
+          position: "top-right",
+          autoClose: 1000,
         });
-       
-        if(data?.status){
-            toast.success(data?.message,{
-                position:"top-right",
-                autoClose:1000
-              });
 
-              onClose(false);
-              refresh();
-        }else{
-            toast.error(data?.message,{
-                position:"top-right",
-                autoClose:1000
-              });
-        }
-         
-
-        
+        onClose(false);
+        refresh();
+      } else {
+        toast.error(data?.message, {
+          position: "top-right",
+          autoClose: 1000,
+        });
+      }
     } catch (error) {
-        toast.error(error?.message, {
-            position: "top-right",
-            autoClose: 1000,
-          });
+      toast.error(error?.message, {
+        position: "top-right",
+        autoClose: 1000,
+      });
     }
-  }
+  };
 
-  const handleChnage=()=>{
-      
-  }
+  const handleChnage = () => {};
 
   return (
     <div className="fixed  top-0 bottom-0 left-0  right-0 w-full h-full z-10 flex justify-between items-center bg-slate-200 bg-opacity-50 ">
       <div className="mx-auto bg-white  shadow-md p-4 w-full max-w-sm">
-        <button className="block ml-auto rounded-full bg-red-400 p-2" onClick={()=>onClose(false)}>
-        <IoMdClose />
+        <button
+          className="block ml-auto rounded-full bg-red-400 p-2"
+          onClick={() => onClose(false)}
+        >
+          <IoMdClose />
         </button>
 
         <h1>Change User Role</h1>
@@ -72,15 +69,20 @@ const Role = ({users,onClose,refresh}) => {
 
         <div className="flex items-center justify-between">
           <p>Role : {userRole} </p>
-          <select className="border px-4 py-1" value={userRole} onChange={(e)=>setUserRole(e.target.value)}>
+          <select
+            className="border px-4 py-1"
+            value={userRole}
+            onChange={(e) => setUserRole(e.target.value)}
+          >
             <option value={roleCheck?.admin}>Admin</option>
             <option value={roleCheck?.user}>User</option>
           </select>
         </div>
 
-        <button className="w-fit mx-auto block border py-1 px-3 rounded-full bg-red-500 hover:bg-red-600"
+        <button
+          className="w-fit mx-auto block border py-1 px-3 rounded-full bg-red-500 hover:bg-red-600"
           onClick={updateUser}
-        > 
+        >
           Chnage Role
         </button>
       </div>
